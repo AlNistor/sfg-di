4f9075b7b3537372420fcc106e09eba253089813
Alex Nistor <alex.nistor@assist.ro>
2020-04-15 15:53:09 +0300

    Added primary bean

--- src/main/java/self/edu/sfgdi/controllers/MyController.java
+++ src/main/java/self/edu/sfgdi/controllers/MyController.java
@@ -1,11 +1,17 @@
 package self.edu.sfgdi.controllers;
 
 import org.springframework.stereotype.Controller;
+import self.edu.sfgdi.services.GreetingService;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     public String sayHello() {
-        return "Hi folks";
+        return greetingService.sayGreeting();
     }
 }
